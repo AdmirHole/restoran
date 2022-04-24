4cbf6565ce400366c3ff7c01699608c3b58d79f8
AdmirHole <53060791+AdmirHole@users.noreply.github.com>
2022-04-24 20:18:43 +0200

    test

--- classes/User.class.php
+++ classes/User.class.php
@@ -20,31 +20,50 @@ class User extends Connection
     {
         $this->Connect();
     }
-    public function insert_grupe($nazivGrupe){
+    public function insert_grupe($nazivGrupe)
+    {
         $this->Connect();
 
-        if($this->conn->query("INSERT INTO grupe (naziv_grupe) VALUES ('$nazivGrupe')")){
+        if ($this->conn->query("INSERT INTO grupe (naziv_grupe) VALUES ('$nazivGrupe')")) {
             return true;
         };
     }
-    public function insert_artikla($idGrupe, $nazivArtikla, $cijena, $kolicina){
+    public function insert_artikla($idGrupe, $nazivArtikla, $cijena, $kolicina)
+    {
         $this->Connect();
 
-        if($this->conn->query("INSERT INTO artikli (naziv, kolicina, cijena, grupa) VALUES ('$nazivArtikla', '$kolicina', '$cijena', '$idGrupe')")){
+        if ($this->conn->query("INSERT INTO artikli (naziv, kolicina, cijena, grupa) VALUES ('$nazivArtikla', '$kolicina', '$cijena', '$idGrupe')")) {
             return true;
         };
     }
-    public function get_grupe(){
+    public function get_grupe()
+    {
         $this->Connect();
 
-        if($result = $this->conn->query("SELECT * FROM grupe")){
-            return $result;
+        if ($result = $this->conn->query("SELECT * FROM grupe")) {
             $data = [];
-            while ($result) {
-                $data[] = $result->fetch_assoc();
+            while ($row = $result->fetch_assoc()) {
+                $data[] = $row;
             }
             return $data;
         };
     }
+
+    public function get_menu()
+    {
+        $this->Connect();
+        if ($result = $this->conn->query("SELECT a.id as artikal_id, a.naziv, a.kolicina, a.cijena, g.naziv_grupe
+        FROM artikli a JOIN grupe g on g.id=a.grupa")) {
+            $data = [];
+            $menu = [];
+            while ($row = $result->fetch_assoc()) {
+                $data[$row['naziv_grupe']][] = $row;
+            }
+
+            return $data;
+        };
+    }
+
+
     //END
 }
--- pages/index.page.php
+++ pages/index.page.php
@@ -1,3 +1,46 @@
-<div>
-<img src="https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=https:\\facebook.ba&choe=UTF-8" title="Link to Google.com" />
+<?php
+require_once "./classes/User.class.php";
+$class = new User();
+$grupe = $class->get_menu();
+?>
+<div class="container">
+    <div class="wrapper">
+        <div class="logo"><img src="./dist/images/oscar.jpg" alt="" class="image"></div>
+        <table class="table">
+            <?php 
+            foreach($grupe as $key => $value){
+                echo "<thead>
+                        <tr>
+                            <th scope='col'>{$key}</th>
+                            <th scope='col'></th>
+                        </tr>
+                    </thead><tbody>";
+                    foreach($value as $key){
+                        echo "
+                        <tr>
+                            <td>{$key['naziv']}</td>
+                            <td>{$key['cijena']} KM</td>
+                        </tr>
+                        ";
+                    }
+                    echo "</tbody>";
+            }
+            ?>
+            
+            
+        </table>
+    </div>
 </div>
+
+
+
+
+
+
+
+
+
+<!-- <div>
+<img src="https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=https:\\facebook.ba&choe=UTF-8" title="Link to Google.com" />
+
+</div> -->
